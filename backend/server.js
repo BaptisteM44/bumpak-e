63ecaf022894bcef2d1871d551f5d4d538c8d78d
Baptiste Morvan <bapmorvan@gmail.com>
2023-05-29 10:19:27 +0200

    server

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true })); // Middleware pour traiter les 
 
 // Middleware pour gérer les en-têtes CORS
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "bumpak-e-production.up.railway.app");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
