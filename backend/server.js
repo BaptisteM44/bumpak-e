cad883a3577f64716eb3c86a402091e64740125e
Baptiste Morvan <bapmorvan@gmail.com>
2024-02-05 20:12:41 +0100

    hooks

--- backend/server.js
+++ backend/server.js
@@ -96,7 +96,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://bumpak.fr"); // Autorise uniquement les requêtes de votre domaine frontal
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Autorise uniquement les requêtes de votre domaine frontal
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "OPTIONS") {
