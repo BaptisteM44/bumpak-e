fff55fbb401fe7d7cc767805a69ad220896b49ca
Baptiste Morvan <bapmorvan@gmail.com>
2024-02-08 20:28:01 +0100

    hooks

--- backend/server.js
+++ backend/server.js
@@ -36,26 +36,21 @@ app.use("/api/import", ImportData);
 app.use("/api/products", productRoute); // Assurez-vous que ceci vient avant votre route personnalisée
 
 // Ajoutez la route /api/products-json ici
-// app.get('/api/products/:slug', async (req, res) => {
-//   try {
-//     const { slug } = req.params;
-//     const product = await Product.findOne({ slug: slug });
-//     if (product) {
-//       res.json({
-//         id: product._id.toString(),
-//         price: product.price,
-//         url: `https://bumpak-e-production.up.railway.app/api/products/${slug}`, // L'URL pour accéder à ce produit spécifique en JSON
-//         // Ajoutez ici d'autres champs si nécessaire
-//       });
-//     } else {
-//       res.status(404).send({ message: 'Product not found' });
-//     }
-//   } catch (error) {
-//     console.error("Error fetching product:", error);
-//     res.status(500).send({ message: 'Server error' });
-//   }
-// });
-
+app.get('/api/products-json', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        const productsForSnipcart = products.map(product => ({
+            id: product._id.toString(),
+            price: product.price,
+            url: `https://bumpak.fr/${product.category}/${product.slug}` // Assurez-vous que cela mène à la page de produit correcte
+        }));
+    
+        res.json(productsForSnipcart);
+    } catch (error) {
+        console.error("Erreur lors de la récupération des produits: ", error);
+        res.status(500).send("Erreur serveur");
+    }
+});
 app.post('/api/snipcart/webhooks', async (req, res) => {
   const { items } = req.body;
 
