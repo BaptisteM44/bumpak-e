acd71b72a3db11fbcd120ef350af11a4bfb33942
Baptiste Morvan <bapmorvan@gmail.com>
2024-02-15 12:10:23 +0100

    CORS

--- backend/server.js
+++ backend/server.js
@@ -32,21 +32,35 @@ app.use("/api/products", productRoute); // Assurez-vous que ceci vient avant vot
 
 // Ajoutez la route /api/products-json ici
 // Route pour obtenir les détails d'un produit spécifique en JSON
+// app.get('/api/products/:slug', async (req, res) => {
+//   const { slug } = req.params;
+//   const product = await Product.findOne({ slug: slug });
+//   if (product) {
+//     // Format attendu par Snipcart
+//     res.json({
+//       id: product._id.toString(),
+//       price: product.price,
+//       url: `https://bumpak-e-production.up.railway.app/api/products/${product.slug}`,
+//       name: product.name,
+//       // Ajoutez ici d'autres champs si nécessaire
+//     });
+//   } else {
+//     res.status(404).send({ message: 'Product not found' });
+//   }
+// });
 app.get('/api/products/:slug', async (req, res) => {
-  const { slug } = req.params;
-  const product = await Product.findOne({ slug: slug });
-  // if (product) {
-  //   // Format attendu par Snipcart
-  //   res.json({
-  //     id: product._id.toString(),
-  //     price: product.price,
-  //     url: `https://bumpak-e-production.up.railway.app/api/products/${product.slug}`,
-  //     name: product.name,
-  //     // Ajoutez ici d'autres champs si nécessaire
-  //   });
-  // } else {
-  //   res.status(404).send({ message: 'Product not found' });
-  // }
+  try {
+    const productSlug = req.params.slug;
+    const product = await Product.findOne({ slug: productSlug });
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).send({ message: 'Product not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Server error' });
+  }
 });
 
 app.post('/api/snipcart/webhooks', async (req, res) => {
