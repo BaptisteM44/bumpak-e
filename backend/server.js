0859a41e764ddc3cf8fb27de05c5f39355c84a4d
Baptiste Morvan <bapmorvan@gmail.com>
2024-11-12 01:13:19 +0100

    test products

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.get('/api/products/:slug', async (req, res) => {
     if (product) {
       // Structure de réponse adaptée à Snipcart
       res.json({
-        id: product._id.toString(),
+        id: product.id.toString(),
         name: product.name,
         price: product.price,
         url: `https://bumpak-e-production.up.railway.app/api/products/${product.slug}`,
