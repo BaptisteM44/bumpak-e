8ef6705e53949d3ca9f04da0b7214ebdfcec005d
Baptiste Morvan <bapmorvan@gmail.com>
2024-11-12 02:00:14 +0100

    change ProductRoutes

--- backend/ProductRoutes.js
+++ backend/ProductRoutes.js
@@ -10,4 +10,24 @@ productRoute.get("/", asyncHandler(
         res.json(products);
     })
 );
+productRoute.get("/:slug", asyncHandler(
+    async (req, res) => {
+      const product = await Product.findOne({ slug: req.params.slug });
+      if (product) {
+        res.json({
+          id: product._id.toString(), // Assurez-vous que l'ID est formaté en `id` pour Snipcart
+          name: product.name,
+          price: product.price,
+          description: product.description,
+          image: product.image,
+          category: product.category,
+          subcategory: product.subcategory,
+          slug: product.slug,
+          url: `https://bumpak-e-production.up.railway.app/api/products/${product.slug}`, // URL pour Snipcart
+        });
+      } else {
+        res.status(404).send({ message: "Product not found" });
+      }
+    })
+  );
 export default productRoute;
